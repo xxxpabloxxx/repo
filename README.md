323b0fb831bac63caab214ef4af7cb8aa4515ec6
xxxpabloxxx <repin.danil2014@gmail.com>
2023-05-03 21:59:54 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,103 @@
-# hishi.github.io
+<div align="center">
+    <img src="https://azu0609.github.io/repo/repo.logo.png">
+    <h1>azu0609's repo</h1>
+    <p>Personalized repo for me. hosted in github pages. feel free to send pr!</p>
+    <hr />
+    <a href="#"><img src="https://img.shields.io/badge/actions-actions.svg?label=Github&logo=github&style=for-the-badge&color=70a5eb" /></a>
+</div>
+
+> **Warning**
+> If scarlet is keep crashing after adding this repo, please submit a issue! there is an scarlet-side bug but it caused by bad repo.
+
+## Included IPA:
+
+| String | Description |
+|:------:|:-----------:|
+| A | Altstore |
+| S | Scarlet |
+
+| Type | Application | Bundle | Base | Version | Author | Working? | Availability |
+|:----:|:----------:|:------:|:------------:|:-------:|:------:|:------:|:------:|
+| Tweaked app | [uYou](https://github.com/MiRO92/uYou-for-YouTube) | com.google.ios.youtube | 18.14.1 | 3.0 | [MIRO92](https://github.com/MiRO92) | Semi(A/S) | A/S |
+| Tweaked app | [uYou+](https://github.com/qnblackcat/uYouPlus) | com.google.ios.youtube | 18.14.1-3.0 | 3.0 | [qnblackcat](https://github.com/qnblackcat) | Yes(any) | A/S |
+| Tweaked app | [Enmity](https://enmity.app) | com.hammerandchisel.discord | 158.0 | v2.2.5 | [enmity-mod](https://github.com/enmity-mod) | Yes(any) | A/S |
+| Tweaked app | [Rosiecord (Plumpy - GGSans)](https://github.com/acquitelol/rosiecord) | com.hammerandchisel.discord | v2.1.4 | 17843158 | [acquitelol](https://github.com/acquitelol) | Semi(S) | A/S |
+| Sideloaded | [Anime Now!](https://github.com/AnimeNow-Team/AnimeNow) | com.errorerrorerror.animenow | any | 3.0 | [errorerrorerror](https://github.com/errorerrorerror) | Yes(any) | A/S |
+| MacDirtycow | [Cowabunga](https://github.com/leminlimez/Cowabunga) | com.leemin.Cowabunga | any | 10.1.1 | [leminlimez](https://github.com/leminlimez) | Yes(any) | A/S |
+| MacDirtycow | [KillMyOTA](https://github.com/haxi0/KillMyOTA) | org.haxi0.KillMyOTA | any | 1.1 | [haxi0](https://github.com/haxi0) | Yes(any) | A/S |
+| MacDirtyCow | [ControlConfig](https://github.com/BomberFish/ControlConfig) | com.f1shy-dev.ControlConfig | any | 0.1.0-alpha | [BomberFish](https://github.com/BomberFish) | Yes(any) | A/S |
+| MacDirtycow | [SantanderEscaped](https://github.com/haxi0/SantanderEscaped) | org.haxi0.santander | any | 0.4 | [SerenaKit](https://github.com/SerenaKit/Santander) / [haxi0](https://github.com/haxi0) | Yes(any) | A/S |
+| MacDirtyCow | [AppCommander](https://github.com/BomberFish/AppCommander) | ca.bomberfish.AppCommander.Dev | any | v0.1 Beta 3 | [BomberFish](https://github.com/BomberFish) | Yes(any) | A/S |
+
+## Add apps to repo
+1. Add json data to apps like:  
+    Scarlet:
+    ```
+    {
+        "name": "Name of application",
+        "version": "0.1",
+        "down": "link_to_file",
+        "dev": "Cool developer name",
+        "icon": "link_to_app_icon",
+        "category": "category",
+        "description": "Description of application",
+        "bundleID": "app.bundle.id",
+        "screenshots" [
+            "link_to_screenshot1",
+            "link_to_screenshot2",
+            "link_to_screenshot3"
+        ],
+        "contact": {
+            "web": "developer_website",
+            "twitter": "https://twitter.com/developer"
+        }
+    }
+    ```
+    For more value in scarlet json manifest, see [Scarlet official repo](https://usescarlet.com/scarlet.json).
+    
+    Altstore:
+    ```
+    {
+        "name": "Name of app",
+        "bundleIdentifier": "app.bundle.id",
+        "developerName": "Cool developer name",
+        "subtitle": "Subtitle (Shows on below of title, 0~15 recommend)",
+        "version": "0.2",
+        "versionDate": "date-of-release",
+        "versionDescription": "Description of version (like what's changed, this part replaced with release body)",
+        "downloadURL": "link_to_file",
+        "versions": [
+            {
+                "version": "0.2",
+                "date": "yyyy-mm-dd",
+                "localizedDescription": "Description of version (like what's changed, this part replaced with release body)",
+                "downloadURL": "link_to_file_of_this_release",
+                "size": 1
+            },
+            {
+                "version": "0.1",
+                "date": "yyyy-mm-dd",
+                "localizedDescription": "Description of version (like what's changed, this part replaced with release body)",
+                "downloadURL": "link_to_file_of_this_release",
+                "size": 1
+            }
+        ],
+        "localizedDescription": "Long description of this app. Show off how cool is!",
+        "iconURL": "link_to_app_icon",
+        "size": 1,
+        "beta": false
+    }
+    ```
+
+    2. Add release source link to fetch.py's self.release_source in project root directory like (this step deprecated in soon):
+    ```
+    self.release_source = ["https://api.github.com/repos/app_repository_owner1/repositry_name1/releases",
+                            "https://api.github.com/repos/app_repository_owner2/repositry_name2/releases",
+                            "https://api.github.com/repos/app_repository_owner3/repositry_name3/releases"]
+    ```
+    For more infomation about altstore json manifest, see [Make a Source](https://faq.altstore.io/sources/make-a-source).
+
+
+3. Check if working correctly by running script:  
+`python3 fetch.py --production`  
+Script should show current version, latest version and current json index then try to update if working correctly.
---
+++ script/fetch.py
@@ -0,0 +1,218 @@
+import requests
+import json
+import sys
+import re
+
+
+class RateLimited(Exception):
+    pass
+
+
+class Fetch:
+    def __init__(self):
+        self.release_source = ["https://api.github.com/repos/acquitelol/rosiecord/releases",
+                               "https://api.github.com/repos/enmity-mod/loader-ios/releases",
+                               "https://api.github.com/repos/qnblackcat/uYouPlus/releases",
+                               "https://api.github.com/repos/AnimeNow-Team/AnimeNow/releases",
+                               "https://api.github.com/repos/leminlimez/Cowabunga/releases",
+                               "https://api.github.com/repos/haxi0/KillMyOTA/releases",
+                               "https://api.github.com/repos/BomberFish/ControlConfig/releases",
+                               "https://api.github.com/repos/haxi0/SantanderEscaped/releases",
+                               "https://api.github.com/repos/BomberFish/AppCommander/releases"]
+        
+        self.blacklist_release = [
+            {
+                "name": "uYou",
+                "version": "2.1",
+                "state": "invalid"
+            },
+            {
+                "name": "Cowabunga",
+                "version": "10",
+                "state": "invalid"
+            }
+        ]
+
+
+    def logger(self, type: int, message: str):
+        if type == 0:
+            print('\033[92m' + "INFO: " + message)
+        elif type == 1:
+            print('\033[96m' + "DEBUG: " + message)
+        elif type == 2:
+            print('\033[93m' + "WARN: " + message)
+        elif type == 3:
+            print('\033[91m' + "ERROR: " + message)
+        else:
+            print(message)
+
+
+    def compare_versions(self, current_ver, new_ver):
+        current_parts = current_ver.split('.')
+        new_parts = new_ver.split('.')
+        for i in range(len(new_parts)):
+            if i >= len(current_parts):
+                return True
+            if new_parts[i] != current_parts[i]:
+                return new_parts[i] > current_parts[i]
+        if len(new_parts) > len(current_parts):
+            return True
+        elif len(new_parts) == len(current_parts):
+            new_suffix = new_ver.split('.')[-1].split('-')[-1]
+            current_suffix = current_ver.split('.')[-1].split('-')[-1]
+            if new_suffix != '' and current_suffix != '':
+                return new_suffix > current_suffix
+            else:
+                return new_ver > current_ver
+        return False
+
+
+    def fetch(self, repo: str, app_name: str, index: int, app_type: str, current_ver, current_download_url: str, current_size=None):
+        version = None
+        changelog = None
+        released_date = None
+        size = None
+        download_url = None
+        current_name_no_version = None
+        asset_name_no_version = None
+        if index == 3 and app_type == "Tweaked" and repo == "scarlet" or index == 3 and repo == "altstore":
+            version = "3.0"
+            released_date = "2023-04-16"
+            changelog = "Added:\nSupport for rootless jailbreak\nDefault playback speed.\nRemoved ads from search results.\nAuto fullscreen in YT player.\nHide upgrade dialog.\nDisabled captions in the muted player.\nDownload original thumbnails.\nAbility to hide uYou button in Shorts.\nAbility to hide title and channel name in Shorts.\nAbility to hide channel avatar in Shorts.\nAbility to hide like button in Shorts.\nAbility to hide dislike button in Shorts.\nAbility to hide comment button in Shorts.\nAbility to hide remix button in Shorts.\nAbility to hide share button in Shorts.\nAbility to fullscreen in Shorts.\nReserved speed rate when changing it until closing YouTube.\nFixes:\nRewrote downloading class for faster downloads.\nFixed downloading errors in sideloaded versions.\nFixed login issues for sideloaded version.\nFixed cast button not hiding.\nFixed uYou's tab bar button.\nFixed all uYou's player bugs.\nFixed sorting downloaded media.\nFixed uYou player UI in RTL languages.\nFixed downloading Shorts.\nRemoved playlist download.\nRemoved uPIP."
+            size = 126877696
+            download_url = "https://miro92.com/repo/depictions/com.miro.uyou/iPA/YouTube_18.14.1_uYou_3.0.ipa",
+            self.logger(2, f"uYou detected in following index: {index}! using 3.0 instead of latest as doesn't have any release system.")
+        else:
+            for i, releases in enumerate(self.release_source):
+                if releases.replace("api.", "").replace("repos/", "") in current_download_url:
+                    req = requests.get(self.release_source[i])
+                    if req.status_code == 403:
+                        self.logger(3, "Looks like Github limited this ip.")
+                        raise RateLimited("Rate limited by Github")
+                    req = req.json()
+                    for release in req:
+                        target_release = release["name"]
+                        if not re.match(fr"^{app_name} (\d+)[\s()]+.*$", target_release):
+                            current_filename = re.search(r"(?<=/)[^/]+$", current_download_url)
+                            pattern = re.compile(r"^(.+?)[\-_\.]\d+[\-_\.](.+)\.([^.]+)$")
+                            release_name_match = re.match(r"(.+)\s\(\w+\)$", target_release)
+                            if release_name_match is not None:
+                                continue
+                            for asset in release["assets"]:
+                                asset_name_match = pattern.match(asset["name"])
+                                current_name_match = pattern.match(current_filename.group())
+                                try:
+                                    asset_name_no_version = asset_name_match.group(1) + "-" + asset_name_match.group(2) + "." + asset_name_match.group(3)
+                                    current_name_no_version = current_name_match.group(1) + "-" + current_name_match.group(2) + "." + current_name_match.group(3)
+                                except AttributeError:
+                                    pass
+                                if asset_name_match and current_name_match or asset["name"] == current_filename.group():
+                                    if asset_name_no_version is not None and current_name_no_version is not None and asset_name_no_version == current_name_no_version or index == 2 or asset["name"] == current_filename.group():
+                                        if version is None: version = release["name"].strip(app_name).strip("v").strip()
+                                        if changelog is None: changelog = release["body"].replace('"', "'")
+                                        if released_date is None: released_date = ''.join(asset["created_at"].split('T')[:-1])
+                                        if size is None: size = asset["size"]
+                                        if download_url is None: download_url = asset["browser_download_url"].replace("%2B", "+")
+                                        break
+                                
+        self.logger(1, f"index: {index}, current: {current_ver}, new: {version}")
+        if self.compare_versions(current_ver, version) or repo == "altstore" and size != current_size:
+            self.logger(0, f"New version available: {version}, verifing compatibility...")
+            for blocked in self.blacklist_release:
+                if blocked["name"] == app_name and blocked["version"] == version and blocked["state"] == "valid":
+                    self.logger(2, f"NG: {app_name}+{version} - In blacklist")
+                else:
+                    self.logger(0, f"OK - Parsing to rw")
+                    self.rw(repo, "../altstore_repo.json" if repo == "altstore" else "../scarlet_repo.json", current_ver, version, download_url, int(index), app_type, changelog, released_date, size)
+                break
+        else:
+            self.logger(0, "Up to date. Nothing to do.")
+
+    
+    def rw(self, repo_type, path, current_ver, version, download_url: str, index, app_type, version_description, release_date, size):
+            self.logger(1, "Fetching repo data...")
+            with open(path, "r") as repo_path:
+                self.json_data = json.load(repo_path)
+                self.logger(1, "Modifying loaded data...")
+                if repo_type == "scarlet":
+                    self.json_data[app_type][index]["version"] = version
+                    self.json_data[app_type][index]["down"] = download_url
+                
+                elif repo_type == "altstore":
+                    self.json_data[app_type][index]["version"] = version
+                    self.json_data[app_type][index]["downloadURL"] = download_url
+                    self.json_data[app_type][index]["versionDescription"] = version_description
+                    self.json_data[app_type][index]["versionDate"] = release_date
+                    self.json_data[app_type][index]["size"] = size
+                    try:
+                        self.json_data[app_type][index]["versions"].insert(0, {"version": version,
+                                                                        "date": release_date,
+                                                                        "localizedDescription": version_description,
+                                                                        "downloadURL": download_url,
+                                                                        "size": size})
+                    except KeyError:
+                        self.logger(2, "Looks like this app doesn't have versions key. adding...")
+                        self.json_data[app_type][index]["versions"] = [
+                            {
+                            "version": version,
+                            "date": release_date,
+                            "localizedDescription": version_description,
+                            "downloadURL": download_url,
+                            "size": size
+                            }
+                        ]
+
+                else:
+                    raise Exception("Unexpected mode!")
+     
+                with open("../README.md", "r") as file:
+                    self.logger(0, "Fetching readme.md data...")
+                    data = file.read()
+                
+                with open("../README.md", "w") as file:
+                    file.write(data.replace(current_ver, version))
+
+                with open(path, "w") as repo_path:
+                    json.dump(self.json_data, repo_path, indent=2)
+                
+                self.logger(0, f"Writing process ended without any error")
+            
+
+    def automate(self, path: str):
+        with open(path, 'r') as file:
+            json_data = json.load(file)
+            for item in json_data:
+                for i, key in enumerate(json_data[item]):
+                    try:
+                        if path == "../altstore_repo.json":
+                            self.fetch("altstore", key["name"], i, item, key["version"], key["downloadURL"], key["size"])
+                        elif path == "../scarlet_repo.json":
+                            self.fetch("scarlet", key["name"], i, item, key["version"], key["down"])
+                        elif path == "../README.md":
+                            self.fetch("readme", key["name"], i, item, key["version"], key["downloadURL"])
+                        else:
+                            raise Exception("Unexpected repo!")
+                    except TypeError as e:
+                        if str(e) == "string indices must be integers, not 'str'" or "string indices must be integers":
+                            pass
+                        else:
+                            raise TypeError(e)
+        self.logger(0, f"Updating process ended successfully.")
+
+
+if __name__ == "__main__":
+    try:
+        try:
+            if sys.argv[1] == "--production" and sys.argv[2] == "altstore":
+                Fetch().automate("../altstore_repo.json")
+            elif sys.argv[1] == "--production" and sys.argv[2] == "scarlet":
+                Fetch().automate("../scarlet_repo.json")
+        except IndexError:
+            if sys.argv[1] == "--production":
+                Fetch().automate("../altstore_repo.json")
+                Fetch().automate("../scarlet_repo.json")
+        print('\033[92m' + f"All done! may take 1~2m(Page build time) to apply.")
+        if sys.argv[1] == "--test":
+            Fetch().fetch(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7])
+    except IndexError:
+        print("ERROR: Needed argument not found. example: --production")
